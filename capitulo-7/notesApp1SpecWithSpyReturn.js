ef57e52ebbdbf42552ca79ba105f5427686e8080
Leonardo Silva <s.leosoares@gmail.com>
2015-03-03 00:33:10 -0300

    Exemplo 7 - Spies

---
+++ capitulo-7/notesApp1SpecWithSpyReturn.js
@@ -0,0 +1,16 @@
+describe('ItemCtrl with SpyReturn', function() {
+  beforeEach(module('notesApp1'));
+
+  var ctrl, itemService;
+  beforeEach(inject(function($controller, ItemService) {
+    spyOn(ItemService, 'list').and.returnValue([{id: 1, label: 'Mock'}]);
+    itemService = ItemService;
+    ctrl = $controller('ItemCtrl');
+  }));
+
+  it('should load mocked out items', function() {
+    expect(itemService.list).toHaveBeenCalled();
+    expect(itemService.list.calls.count()).toEqual(1);
+    expect(ctrl.items).toEqual([{id: 1, label: 'Mock'}]);
+  });
+});
