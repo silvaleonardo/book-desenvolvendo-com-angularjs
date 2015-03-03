b6bdc7ff066aa998c6845664f923923582e726d8
Leonardo Silva <s.leosoares@gmail.com>
2015-03-03 00:02:12 -0300

    Exemplo 4 - Simulando os servicos

---
+++ capitulo-7/notesApp1Spec.js
@@ -0,0 +1,21 @@
+describe('ItemCtrl with inline mock', function() {
+  beforeEach(module('notesApp1'));
+
+  var ctrl, mockService;
+  beforeEach(module(function($provide) {
+    mockService = {
+      list: function () {
+        return [{id: 1, label: 'Mock'}];
+      }
+    };
+    $provide.value('ItemService', mockService);
+  }));
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ItemCtrl');
+  }));
+
+  it('should load mocked out items', function() {
+    expect(ctrl.items).toEqual([{id: 1, label: 'Mock'}]);
+  });
+});
