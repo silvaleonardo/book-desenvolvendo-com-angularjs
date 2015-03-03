e78498c5ff33dfcbf4267ebf12e1bf6db435a6ef
Leonardo Silva <s.leosoares@gmail.com>
2015-03-02 23:47:53 -0300

    Exemplo 3 - Estados entre testes de unidade

---
+++ capitulo-7/simpleCtrl2.js
@@ -0,0 +1,10 @@
+angular.module('simpleCtrl2App', [])
+       .controller('SimpleCtrl2', ['$location', '$window', function($location, $window) {
+        var self = this;
+        self.navigate1 = function() {
+          $location.path('/some/where');
+        };
+        self.navigate2 = function() {
+          $location.path('/some/where/else');
+        };
+       }]);
