caa058cf7850e9cb1459d40e998dcc833459eb4b
Leonardo Silva <s.leosoares@gmail.com>
2015-03-03 00:25:55 -0300

    Exemplo 6 - Spies

---
+++ capitulo-7/notesApp1SpecWithSpies.js
@@ -0,0 +1,19 @@
+describe('ItemCtrl with spies', function() {
+  beforeEach(module('notesApp1'));
+
+  var ctrl, itemService;
+  beforeEach(inject(function($controller, ItemService) {
+    spyOn(ItemService, 'list').and.callThrough();
+    itemService = ItemService;
+    ctrl = $controller('ItemCtrl');
+  }));
+
+  it('should load mocked out items', function() {
+    expect(itemService.list).toHaveBeenCalled();
+    expect(itemService.list.calls.count()).toEqual(1);
+    expect(ctrl.items).toEqual([
+      {id: 1, label: 'Item 0'},
+      {id: 2, label: 'Item 1'}
+    ]);
+  });
+});
