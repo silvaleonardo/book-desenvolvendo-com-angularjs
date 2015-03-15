a69cd8f0d63d355199d959a3e540cb13966f007e
Leonardo Silva <s.leosoares@gmail.com>
2015-03-15 00:10:03 -0300

    Exemplo 2 - Testando o filtro timeAgo

---
+++ capitulo-9/timeAgoFilterOptionalArgumentSpec.js
@@ -0,0 +1,29 @@
+describe('timeAgo Filter', function() {
+  beforeEach(module('filtersApp'));
+
+  var filter;
+  beforeEach(inject(function(timeAgoFilter) {
+    filter = timeAgoFilter;
+  }));
+
+  it('should respond based on timestamp', function() {
+    // A presença de new Date().getTime() dificulta um pouco
+    // realizar o teste de unidade de forma determinística.
+    // O ideal seria injetar um dateProvider no filtro timeAgo,
+    // mas estamos tentando manter a simplicidade nesse caso.
+    // Portanto iremos supor que nossos testes são rápidos e suficiente
+    // para serem executados em questão de milissegundos.
+
+    var currentTime = new Date().getTime();
+    currentTime -= 10000;
+    expect(filter(currentTime, false)).toEqual('minutes ago');
+    var fewMinutesAgo = currentTime - 1000 * 60;
+    expect(filter(fewMinutesAgo, false)).toEqual('minutes ago');
+    var fewHoursAgo = currentTime - 1000 * 60 * 68;
+    expect(filter(fewHoursAgo, false)).toEqual('hours ago');
+    var fewDaysAgo = currentTime - 1000 * 60 * 60 * 26;
+    expect(filter(fewDaysAgo, false)).toEqual('days ago');
+    var fewMonthsAgo = currentTime - 1000 * 60 * 60 * 24 * 32;
+    expect(filter(fewMonthsAgo, false)).toEqual('months ago');
+  });
+});
