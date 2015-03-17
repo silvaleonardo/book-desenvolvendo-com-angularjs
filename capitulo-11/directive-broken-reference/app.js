62c6c3293282c9bdf75e74fc254deb95a92b3bae
Leonardo Silva <s.leosoares@gmail.com>
2015-03-17 02:02:32 -0300

    Exemplo 7 - Escopo - Broken reference

---
+++ capitulo-11/directive-broken-reference/app.js
@@ -0,0 +1,21 @@
+angular.module('stockMarketApp', [])
+       .controller('MainCtrl', [function() {
+        var self = this;
+        self.stocks = [
+          {name: 'First Stock', price: 100, previous: 220},
+          {name: 'Second Stock', price: 140, previous: 120}
+        ];
+        self.changeAllStocks = function() {
+          for (var i = 0; i < 4; i++) {
+            self.stocks[i] = {
+              name: 'Controller Stock',
+              price: 200,
+              previous: 250
+            };
+          }
+        };
+
+        self.changeFirstStock = function() {
+          self.stocks[0].name = 'Changed First Stock';
+        };
+      }]);
---
+++ capitulo-11/directive-broken-reference/directive.js
@@ -0,0 +1,24 @@
+angular.module('stockMarketApp')
+       .directive('stockWidget', [function() {
+        return {
+          templateUrl: 'stock.html',
+          restrict: 'A',
+          scope: {
+            stockData: '='
+          },
+          link: function($scope, $element, $attrs) {
+            $scope.getChange = function(stock) {
+              return Math.ceil(((stock.price - stock.previous) /
+                  stock.previous) * 100);
+            };
+
+            $scope.changeStock = function() {
+              $scope.stockData = {
+                name: 'Directive Stock',
+                price: 500,
+                previous: 200
+              };
+            };
+          }
+        };
+      }]);
