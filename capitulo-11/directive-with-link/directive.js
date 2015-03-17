b9fc158758f81bb5cd10b0187f8f707773edc579
Leonardo Silva <s.leosoares@gmail.com>
2015-03-17 01:45:31 -0300

    Exemplo 5 - A funcao link

---
+++ capitulo-11/directive-with-link/directive.js
@@ -0,0 +1,13 @@
+angular.module('stockMarketApp')
+       .directive('stockWidget', [function() {
+        return {
+          templateUrl: 'stock.html',
+          restrict: 'AE',
+          link: function($scope, $element, $attrs) {
+	        $scope.getChange = function(stock) {
+	          return Math.ceil(((stock.price - stock.previous) /
+	              stock.previous) * 100);
+	        };
+	      }
+        };
+      }]);
