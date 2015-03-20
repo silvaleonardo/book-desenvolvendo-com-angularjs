139e905b2036a6f887905e19c1ec3f388f1921db
Leonardo Silva <s.leosoares@gmail.com>
2015-03-19 23:06:58 -0300

    Exemplo 1 - Transclusoes

---
+++ capitulo-13/directive-no-transclusion/directive.js
@@ -0,0 +1,16 @@
+angular.module('stockMarketApp')
+       .directive('stockWidget', [function() {
+        return {
+          templateUrl: 'stock.html',
+          restrict: 'A',
+          scope: {
+            stockData: '='
+          },
+          link: function($scope, $element, $attrs) {
+            $scope.getChange = function(stock) {
+              return Math.ceil(((stock.price - stock.previous) /
+                stock.previous) * 100);
+            };
+          }
+        };
+      }]);
