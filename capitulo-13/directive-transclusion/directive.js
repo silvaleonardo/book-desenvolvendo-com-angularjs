8c7505b31495bf5b470d13412273707d12671fb8
Leonardo Silva <s.leosoares@gmail.com>
2015-03-19 23:14:26 -0300

    Exemplo 2 - Transclusao basica

---
+++ capitulo-13/directive-transclusion/directive.js
@@ -0,0 +1,17 @@
+angular.module('stockMarketApp')
+       .directive('stockWidget', [function() {
+        return {
+          templateUrl: 'stock.html',
+          restrict: 'A',
+          transclude: true,
+          scope: {
+            stockData: '='
+          },
+          link: function($scope, $element, $attrs) {
+            $scope.getChange = function(stock) {
+              return Math.ceil(((stock.price - stock.previous) /
+                  stock.previous) * 100);
+            };
+          }
+        };
+      }]);
