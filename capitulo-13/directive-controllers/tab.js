79b61767a066956aae661da434c9d601034634ec
Leonardo Silva <s.leosoares@gmail.com>
2015-03-19 23:45:44 -0300

    Exemplo 4 - Controladores de diretivas e require

---
+++ capitulo-13/directive-controllers/tab.js
@@ -0,0 +1,13 @@
+angular.module('stockMarketApp')
+       .directive('tab', [function() {
+        return {
+          restrict: 'E',
+          transclude: true,
+          template: '<div ng-show="selected" ng-transclude></div>',
+          require: '^tabs',
+          scope: true,
+          link: function($scope, $element, $attr, tabCtrl) {
+            tabCtrl.registerTab($attr.title, $scope);
+          }
+        };
+      }]);
